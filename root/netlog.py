a65d24d757e0f3be7ad2d33a891d23d66081fcb6
fn_share <fn_share@126.com>
2023-03-10 16:24:11 +0800

    23-03-10

--- root/netlog.py
+++ root/netlog.py
@@ -16,6 +16,152 @@ from .ssi_login import verify_auth, ripemd_hash, refresh_periods, WEBSITE_REALM,
 _data_dir = os.environ.get('LOCAL_DIR','./data')
 os.makedirs(_data_dir,exist_ok=True)
 
+#---- wrap files
+
+def md_base_dir(login_sess):
+  s = os.path.join(_data_dir,'netlog',login_sess)
+  os.makedirs(s,exist_ok=True)
+  return s
+
+def ensure_md_edt_file(edt_file, cfg_file):
+  if not os.path.isfile(edt_file):
+    with open(edt_file,'wt') as f:
+      f.write('样例文件\n=======\n\n&nbsp;\n\n### 1. 章标题\n\n#### 1.1 节标题\n\n这是正文\n')
+  if not os.path.isfile(cfg_file):
+    with open(cfg_file,'wt') as f:
+      f.write('{}')
+
+def get_publish_info(login_sess):
+  info = { 'login_session':login_sess, 'content':'', 'modify_at':0 }
+  base_dir = md_base_dir(login_sess)
+  idx_file = os.path.join(base_dir,'index.md')
+  
+  if os.path.isdir(base_dir) and os.path.isfile(idx_file):
+    st = os.stat(idx_file)
+    with open(idx_file,'rb') as f:
+      info['content'] = base64.b64encode(f.read()).decode('utf-8')
+      info['modify_at'] = int(st.st_mtime)
+      info['file_size'] = st.st_size
+  return info
+
+def get_editing_info(login_sess):
+  base_dir = md_base_dir(login_sess)
+  edt_file = os.path.join(base_dir,'editing.md')
+  cfg_file = os.path.join(base_dir,'editing.cfg')
+  ensure_md_edt_file(edt_file,cfg_file)
+  
+  desc = ''; cfg = None; opened = False
+  if os.path.isfile(cfg_file):
+    with open(cfg_file,'rb') as f:
+      cfg = json.load(f)
+  if cfg is not None:
+    opened = bool(cfg.get('opened',0))
+    last_archive = cfg.get('archive_time',0)
+    last_editing = cfg.get('editing_time',0)
+    if not last_archive:
+      desc += '本文尚未发布。\n\n'
+    else:
+      desc += '本文在 %s 最后发布。\n\n' % time.strftime('%y-%m-%d %H:%M:%S',tuple(time.localtime(last_archive)))
+    
+    if not last_editing:
+      desc += '本文尚未提交更新。\n'
+    else:
+      desc += '用户（指纹 %s）于 %s 最后更新。\n' % (cfg.get('last_editor',''),time.strftime('%y-%m-%d %H:%M:%S',tuple(time.localtime(last_editing))))
+    
+    curr_opener = cfg.get('locker_opener','')
+    curr_open_tm = cfg.get('last_open',0)
+    if opened and curr_opener and int(time.time()) - curr_open_tm < _locker_expired:
+      tm_desc = time.strftime('%y-%m-%d %H:%M:%S',tuple(time.localtime(curr_open_tm)))
+      desc += '\n用户（指纹 %s）于 %s 开锁，正在编辑中 ...\n' % (int(curr_opener,16),tm_desc)
+  
+  return (opened, desc)
+
+def get_editing_text(login_sess):
+  base_dir = md_base_dir(login_sess)
+  edt_file = os.path.join(base_dir,'editing.md')
+  cfg_file = os.path.join(base_dir,'editing.cfg')
+  ensure_md_edt_file(edt_file,cfg_file)
+  
+  with open(edt_file,'rb') as f:
+    return f.read()
+
+def put_editing_text(login_sess, by_gncd, figerprint, ctx, now):
+  base_dir = md_base_dir(login_sess)
+  edt_file = os.path.join(base_dir,'editing.md')
+  cfg_file = os.path.join(base_dir,'editing.cfg')
+  ensure_md_edt_file(edt_file,cfg_file)
+  
+  if os.path.isfile(cfg_file):
+    with open(cfg_file,'rb') as f:
+      cfg = json.load(f)
+  else: cfg = {}
+  
+  # when current login by green card, and not same unlock operator, and still before _locker_expired
+  if by_gncd and cfg.get('locker_opener') != figerprint and (now - cfg.get('last_open',0)) < _locker_expired:
+    return 'UNLOCK_BY_OTHER'
+  
+  with open(edt_file,'wb') as f:
+    with open(cfg_file,'wt') as f2:
+      cfg['last_editor'] = int(figerprint,16)
+      cfg['editing_time'] = now
+      json.dump(cfg,f2)
+    f.write(ctx)
+  return 'OK'
+
+def modify_locker(action, login_sess, by_gncd, figerprint, now):
+  base_dir = md_base_dir(login_sess)
+  edt_file = os.path.join(base_dir,'editing.md')
+  cfg_file = os.path.join(base_dir,'editing.cfg')
+  ensure_md_edt_file(edt_file,cfg_file)
+  
+  cfg = None; need_save = False
+  with open(cfg_file,'rt') as f:
+    cfg = json.load(f)
+    if cfg.get('opened'):  # already opened
+      # less than 0x80 means not login by meta-passport
+      if by_gncd and cfg.get('locker_opener') != figerprint and (now-cfg.get('last_open',0)) < _locker_expired:
+        return 'UNLOCK_BY_OTHER'   # login by green card, and not same unlock operator, and still before _locker_expired
+    
+    if action == 'open_locker':
+      if not cfg.get('opened'):
+        cfg['opened'] = True   # open it
+        cfg['locker_opener'] = figerprint
+        cfg['last_open'] = int(time.time())
+        need_save = True
+      # else, already unlocked
+    else:   # 'close_locker'
+      if cfg.get('opened'):
+        cfg['opened'] = False  # close it
+        need_save = True
+      # else, already locked
+  
+  if need_save:
+    with open(cfg_file,'wt') as f:
+      json.dump(cfg,f)
+  return 'OK'
+
+def publish_editing_text(login_sess, now):
+  base_dir = md_base_dir(login_sess)
+  idx_file = os.path.join(base_dir,'index.md')
+  edt_file = os.path.join(base_dir,'editing.md')
+  cfg_file = os.path.join(base_dir,'editing.cfg')
+  ensure_md_edt_file(edt_file,cfg_file)
+  
+  cfg = json.load(open(cfg_file,'rt'))
+  if cfg.get('archive_time',0) > cfg.get('editing_time',now):
+    return 'NO_CHANGE'
+  
+  with open(edt_file,'rb') as f:
+    with open(idx_file,'wb') as f2:
+      f2.write(f.read())
+  
+  cfg['archive_time'] = now
+  with open(cfg_file,'wt') as f:
+    json.dump(cfg,f)
+  return 'OK'
+
+#----
+
 _route_prefix = os.environ.get('ROUTE_PREFIX','')
 
 _real_website = ''
@@ -23,11 +169,6 @@ _app_admin_pubkey = ''
 _app_strategy_str = '{}'
 
 _locker_expired = 86400    # default is 1 day, would config as: refresh_period*(session_limit+1)
-
-def md_base_dir(login_sess):
-  s = os.path.join(_data_dir,'netlog',login_sess)
-  os.makedirs(s,exist_ok=True)
-  return s
 
 @app.route(_route_prefix+'/')
 @app.route(_route_prefix+'/index.html')
@@ -40,19 +181,8 @@ def netlog_index_page():
 @app.route(_route_prefix+'/md/<login_sess>')
 def get_netlog_md(login_sess):
   try:
-    info = { 'login_session':login_sess, 'content':'', 'modify_at':0 }
-    base_dir = md_base_dir(login_sess)
-    idx_file = os.path.join(base_dir,'index.md')
-    
-    if os.path.isdir(base_dir) and os.path.isfile(idx_file):
-      st = os.stat(idx_file)
-      with open(idx_file,'rb') as f:
-        info['content'] = base64.b64encode(f.read()).decode('utf-8')
-        info['modify_at'] = int(st.st_mtime)
-        info['file_size'] = st.st_size
-    
+    info = get_publish_info(login_sess)
     return render_template('netlog_show_md.html',info=info)
-  
   except:
     logger.warning(traceback.format_exc())
   return ('FORMAT_ERROR',400)
@@ -60,14 +190,6 @@ def get_netlog_md(login_sess):
 @app.route(_route_prefix+'/visa/<card_hash>')
 def get_netlog_visa(card_hash):
   return render_template('netlog_fetch_visa.html',info={'hash':card_hash})
-
-def ensure_md_edt_file(edt_file, cfg_file):
-  if not os.path.isfile(edt_file):
-    with open(edt_file,'wt') as f:
-      f.write('样例文件\n=======\n\n&nbsp;\n\n### 1. 章标题\n\n#### 1.1 节标题\n\n这是正文\n')
-  if not os.path.isfile(cfg_file):
-    with open(cfg_file,'wt') as f:
-      f.write('{}')
 
 @app.route(_route_prefix+'/stat')
 def get_netlog_stat():
@@ -83,38 +205,11 @@ def get_netlog_stat():
     # step 2: get login_sess and stat resource
     login_sess = sdat[2:2+ord(sdat[1:2])]
     assert len(login_sess) == 20
+    
     login_sess2 = base36.b36encode(login_sess).decode('utf-8')
+    opened, desc = get_editing_info(login_sess2)
     
-    base_dir = md_base_dir(login_sess2)
-    edt_file = os.path.join(base_dir,'editing.md')
-    cfg_file = os.path.join(base_dir,'editing.cfg')
-    ensure_md_edt_file(edt_file,cfg_file)
-    
-    desc = ''; cfg = None; opened = False
-    if os.path.isfile(cfg_file):
-      with open(cfg_file,'rb') as f:
-        cfg = json.load(f)
-    if cfg is not None:
-      opened = bool(cfg.get('opened',0))
-      last_archive = cfg.get('archive_time',0)
-      last_editing = cfg.get('editing_time',0)
-      if not last_archive:
-        desc += '本文尚未发布。\n\n'
-      else:
-        desc += '本文在 %s 最后发布。\n\n' % time.strftime('%y-%m-%d %H:%M:%S',tuple(time.localtime(last_archive)))
-      
-      if not last_editing:
-        desc += '本文尚未提交更新。\n'
-      else:
-        desc += '用户（指纹 %s）于 %s 最后更新。\n' % (cfg.get('last_editor',''),time.strftime('%y-%m-%d %H:%M:%S',tuple(time.localtime(last_editing))))
-      
-      curr_opener = cfg.get('locker_opener','')
-      curr_open_tm = cfg.get('last_open',0)
-      if opened and curr_opener and int(time.time()) - curr_open_tm < _locker_expired:
-        tm_desc = time.strftime('%y-%m-%d %H:%M:%S',tuple(time.localtime(curr_open_tm)))
-        desc += '\n用户（指纹 %s）于 %s 开锁，正在编辑中 ...\n' % (int(curr_opener,16),tm_desc)
-    
-    return { 'desc':desc, 'path':'md/'+login_sess2, 'opened':opened }
+    return { 'path':'md/'+login_sess2, 'opened':opened, 'desc':desc }
   
   except:
     logger.warning(traceback.format_exc())
@@ -136,40 +231,21 @@ def do_netlog_editing():
     assert len(login_sess) == 20
     login_sess2 = base36.b36encode(login_sess).decode('utf-8')
     
-    base_dir = md_base_dir(login_sess2)
-    edt_file = os.path.join(base_dir,'editing.md')
-    cfg_file = os.path.join(base_dir,'editing.cfg')
-    ensure_md_edt_file(edt_file,cfg_file)
-    
     if request.method == 'GET':
-      with open(edt_file,'rb') as f:
-        return f.read()
+      return get_editing_text(login_sess2)
     
     else:  # request.method == 'POST'
       if role != b'editor' and role != b'manager':
         return ('NOT_SUPPORT',400)
       
-      if os.path.isfile(cfg_file):
-        with open(cfg_file,'rb') as f:
-          cfg = json.load(f)
-      else: cfg = {}
-      
-      # when current login by green card, and not same unlock operator, and still before _locker_expired
-      now = int(time.time())
-      if ord(sdat[:1]) < 0x80 and cfg.get('locker_opener') != sid[:4].hex() and (now - cfg.get('last_open',0)) < _locker_expired:
-        return ('UNLOCK_BY_OTHER',400)
-      
       data = request.get_json(force=True,silent=True)
       ctx = base64.b64decode(data['content'])  # ctx can be '' that means reset content
+      now = int(time.time())
+      ret = put_editing_text(login_sess2,ord(sdat[:1]) < 0x80,sid[:4].hex(),ctx,now)
       
-      with open(edt_file,'wb') as f:
-        with open(cfg_file,'wt') as f2:
-          cfg['last_editor'] = int(sid[:4].hex(),16)  # figerprint
-          cfg['editing_time'] = int(time.time())
-          json.dump(cfg,f2)
-        f.write(ctx)
-      
-      return {'result':'OK'}
+      if ret == 'UNLOCK_BY_OTHER':
+        return (ret,400)
+      else: return {'result':ret}
   
   except:
     logger.warning(traceback.format_exc())
@@ -216,37 +292,10 @@ def post_netlog_locker():
     assert len(login_sess) == 20
     login_sess2 = base36.b36encode(login_sess).decode('utf-8')
     
-    base_dir = md_base_dir(login_sess2)
-    edt_file = os.path.join(base_dir,'editing.md')
-    cfg_file = os.path.join(base_dir,'editing.cfg')
-    ensure_md_edt_file(edt_file,cfg_file)
-    
-    cfg = None; need_save = False
-    with open(cfg_file,'rt') as f:
-      cfg = json.load(f)
-      if cfg.get('opened'):  # already opened
-        # less than 0x80 means not login by meta-passport
-        if ord(sdat[:1]) < 0x80 and cfg.get('locker_opener') != sid[:4].hex() and (now-cfg.get('last_open',0)) < _locker_expired:
-          return ('UNLOCK_BY_OTHER',400)   # login by green card, and not same unlock operator, and still before _locker_expired
-      
-      if action == 'open_locker':
-        if not cfg.get('opened'):
-          cfg['opened'] = True   # open it
-          cfg['locker_opener'] = sid[:4].hex()  # hex figerprint
-          cfg['last_open'] = int(time.time())
-          need_save = True
-        # else, already unlocked
-      else:   # 'close_locker'
-        if cfg.get('opened'):
-          cfg['opened'] = False  # close it
-          need_save = True
-        # else, already locked
-    
-    if need_save:
-      with open(cfg_file,'wt') as f:
-        json.dump(cfg,f)
-    
-    return {'result':'OK'}
+    ret = modify_locker(action,login_sess2,ord(sdat[:1]) < 0x80,sid[:4].hex(),now)
+    if ret == 'UNLOCK_BY_OTHER':
+      return (ret,400)
+    else: return {'result':ret}
   
   except:
     logger.warning(traceback.format_exc())
@@ -292,25 +341,10 @@ def post_netlog_publish():
     assert len(login_sess) == 20
     login_sess2 = base36.b36encode(login_sess).decode('utf-8')
     
-    base_dir = md_base_dir(login_sess2)
-    idx_file = os.path.join(base_dir,'index.md')
-    edt_file = os.path.join(base_dir,'editing.md')
-    cfg_file = os.path.join(base_dir,'editing.cfg')
-    ensure_md_edt_file(edt_file,cfg_file)
-    
-    cfg = json.load(open(cfg_file,'rt'))
-    if cfg.get('archive_time',0) > cfg.get('editing_time',now):
-      return ('NO_CHANGE',400)
-    
-    with open(edt_file,'rb') as f:
-      with open(idx_file,'wb') as f2:
-        f2.write(f.read())
-    
-    cfg['archive_time'] = int(time.time())
-    with open(cfg_file,'wt') as f:
-      json.dump(cfg,f)
-    
-    return {'result':'OK'}
+    ret = publish_editing_text(login_sess2,now)
+    if ret == 'NO_CHANGE':
+      return (ret,400)
+    else: return {'result':ret}
   
   except:
     logger.warning(traceback.format_exc())
